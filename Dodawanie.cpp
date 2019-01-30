09b5ba5fb316f87c66003a77cbe0c049a96dfaca
SajmonLab <szymon.niewczas2@gmail.com>
2019-01-30 19:28:09 +0100

    initial commit

---
+++ Dodawanie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b;
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> b;
+    double c= a+b;
+    cout << "Wynik: "<<c;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Laboratorium
+# Laboratorium narzędzia
